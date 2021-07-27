6fb52c76e0d3ebe7677388860ac28d3daa6d43e5
omipapal12 <omipapal12@gmail.com>
2021-07-27 22:54:36 +0530

    Added: Link List Created

---
+++ src/MyLinkedList.java
@@ -0,0 +1,11 @@
+
+public class MyLinkedList {
+	private INode head;
+    private INode tail;
+    private  int size;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+}
