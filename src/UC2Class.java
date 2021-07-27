f789a27286a545a520e2fa8db90c1eab94a5e3ea
omipapal12 <omipapal12@gmail.com>
2021-07-27 23:06:08 +0530

    Added: Value Added In Linked List

---
+++ src/UC2Class.java
@@ -0,0 +1,16 @@
+
+public class UC2Class {
+	public static void main(String[] args) {
+		
+	INode myFirstNode=new MyNode<Integer>(70);
+    INode mySecondNode=new MyNode<Integer>(30);
+    INode myThirdNode=new MyNode<Integer>(56);
+
+    MyLinkedList obj= new MyLinkedList();
+    obj.add(myFirstNode);
+    obj.add(mySecondNode);
+    obj.add(myThirdNode);
+    obj.printNodes();
+    
+	}
+}
