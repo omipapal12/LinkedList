13605979629a739ff1244fc932c3c0c691fb99a5
omipapal12 <omipapal12@gmail.com>
2021-07-27 23:26:20 +0530

    Added: Pop Method Hass Been Added

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -3,6 +3,8 @@ public class MyLinkedList {
 	private INode head;
     private INode tail;
     private  int size;
+    private  INode next;
+    private INode prev_node;
 
     public MyLinkedList() {
         this.head = null;
@@ -35,4 +37,28 @@ public class MyLinkedList {
         }
         System.out.println(nodes);
     }
+    public void insertAfter(INode node, int new_data)
+    {
+        if (this.head == null)
+        {
+            System.out.println("The given previous node cannot be null");
+            return;
+        }
+
+        INode new_node = new MyNode(new_data);
+        INode a=prev_node.getNext();
+        new_node.setNext(a);
+
+    }
+    public INode pop() {
+        INode temp = this.head;
+
+        this.head = head.getNext();
+
+        size--;
+
+        return temp;
+
+    }
+
 }
