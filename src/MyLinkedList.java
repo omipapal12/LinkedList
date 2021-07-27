3b913d37ac4626b88abb3c4d9efbe723383f6487
omipapal12 <omipapal12@gmail.com>
2021-07-27 23:18:53 +0530

    Added: Append Data In Linked List

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -8,4 +8,31 @@ public class MyLinkedList {
         this.head = null;
         this.tail = null;
     }
+    public void add(INode node) {
+        if (this.head == null) {
+            this.head = node;
+        }
+        if (this.tail == null) {
+            this.tail = node;
+        } else {
+            INode temp = head;
+            this.head = node;
+            this.head.setNext(temp);
+
+        }
+    }
+    public void printNodes() {
+        StringBuilder nodes = new StringBuilder("Node Keys");
+        INode temp = this.head;
+        while (temp != null) {
+            nodes.append(temp.getKey());
+            if (temp != tail) {
+                nodes.append("->");
+
+            }
+            temp = temp.getNext();
+
+        }
+        System.out.println(nodes);
+    }
 }
